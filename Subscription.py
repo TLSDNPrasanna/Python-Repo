92a53d03fd035292eb07d8e19708d8a8eac84508
TLSDNPrasanna <147420598+TLSDNPrasanna@users.noreply.github.com>
2024-10-23 23:21:33 +0530

    Uploading code: Subscription.py

---
+++ Subscription.py
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class sub{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if (n>40)
+        System.out.println("YES");
+        else
+        System.out.println("NO");
+
+    }
+}
