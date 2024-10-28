2fc9f0e08e417d8e820246a806999c0b4f6bfd91
TLSDNPrasanna <147420598+TLSDNPrasanna@users.noreply.github.com>
2024-10-28 22:37:28 +0530

    Uploading code: Arithmetic Operations.py

---
+++ Arithmetic Operations.py
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class AO{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println("Sum:"+(a+b));
+        System.out.println("Difference:"+(a-b));
+        System.out.println("Product:"+(a*b));
+        System.out.println("Quotient:"+(a/b));
+        System.out.println("Remainder:"+(a%b));
+        sc.close();
+    }
+}
