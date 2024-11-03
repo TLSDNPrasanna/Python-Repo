2468ca26957719d15709c6ffefe6295520071350
TLSDNPrasanna <147420598+TLSDNPrasanna@users.noreply.github.com>
2024-11-03 20:36:09 +0530

    Uploading code: Area of a Circle.py

---
+++ Area of a Circle.py
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class area{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        float a=3.14f*r*r;
+        System.out.printf("%.2f",a);
+    }
+}
