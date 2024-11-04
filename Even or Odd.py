89e9a011939bbf5c60b2758dda13bc04555db2b6
TLSDNPrasanna <147420598+TLSDNPrasanna@users.noreply.github.com>
2024-11-04 19:14:56 +0530

    Uploading code: Even or Odd.py

---
+++ Even or Odd.py
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class eo{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        if(m%2==0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+            
+    }
+}
