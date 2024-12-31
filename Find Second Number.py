8b0e801bb0f451513db9f18ed23f1f29337cb5eb
TLSDNPrasanna <147420598+TLSDNPrasanna@users.noreply.github.com>
2024-12-31 15:51:16 +0530

    Uploading code: Find Second Number.py

---
+++ Find Second Number.py
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class num2{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int avg=sc.nextInt();
+        int a=sc.nextInt();
+        System.out.println((2*avg)-a);
+    }
+}
